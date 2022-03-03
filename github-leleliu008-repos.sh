bead8daa744f7700ff101a53dd321fe00d9b9050
leleliu008 <leleliu008@gmail.com>
2022-03-03 18:53:59 +0800

    init

---
+++ github-leleliu008-repos.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -ex
+
+JSON_FILE_ALL='github-leleliu008-repos-all.txt'
+
+if [ -f "$JSON_FILE_ALL" ] ; then
+    mv  "$JSON_FILE_ALL" "$JSON_FILE_ALL.bak.$(date +%s)"
+fi
+
+for i in $(seq 4)
+do
+    JSON_FILE_I="github-leleliu008-repos-$i.json"
+    curl -L -o "$JSON_FILE_I" "https://api.github.com/users/leleliu008/repos?page=$i"
+    jq '.[].name' "$JSON_FILE_I"  | sed 's|"||g' | tee -a "$JSON_FILE_ALL"
+done
+
+wc -l "$JSON_FILE_ALL"
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ex
+
+git config --global user.name  leleliu008
+git config --global user.email leleliu008@gmail.com
+
+cat > ~/.ssh/config <<EOF
+Host *
+    StrictHostKeyChecking no
+EOF
+
+while read repo
+do
+    git clone --bare https://github.com/leleliu008/$repo.git
+    git -C $item.git push --mirror git@gitee.com:fpliu/$repo.git
+done < github-leleliu008-repos-all.txt
