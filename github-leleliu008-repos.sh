85536dfb21b6073c2440f255bb1efc494a52c8fd
leleliu008 <leleliu008@gmail.com>
2024-10-25 20:16:18 +0800

    optimized

--- github-leleliu008-repos.sh
+++ github-leleliu008-repos.sh
@@ -8,7 +8,7 @@ if [ -f "$JSON_FILE_ALL" ] ; then
     mv  "$JSON_FILE_ALL" "$JSON_FILE_ALL.bak.$(date +%s)"
 fi
 
-for i in $(seq 5)
+for i in $(seq 6)
 do
     JSON_FILE_I="github-leleliu008-repos-$i.json"
     curl -L -o "$JSON_FILE_I" "https://api.github.com/users/leleliu008/repos?page=$i"
